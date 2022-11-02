89893878fe53fb9ddd4a3dcfa07c02377d5edab9
singhtushar689 <singhtushar689@gmail.com>
2022-11-02 22:39:16 +0530

    First Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NewProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            human human = new human();
+            human.Hair();
+            human.bodytype();
+        }
+    }
+}
---
+++ human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject
+{
+    class human
+    {
+        public string PersonName;
+        public int PhoneNumber;
+
+        public void Hair()
+        {
+            Console.WriteLine("There are different types of color");
+
+        }
+        public void bodytype()
+        { 
+           Console.WriteLine("There are various types of bodytypes")        
+        }
+    }
+        
+}
+
